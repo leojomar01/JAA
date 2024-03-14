af1ccfa7b046be260b5a9897195a901d1a11a281
Leo Jomar Reyes <105467483+leojomar01@users.noreply.github.com>
2024-03-14 22:11:07 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const ToDoList = require('./models/ToDoList');
 const app = express();
 app.use(cors(
     {
-        origin:["https://jaa-e5cw.vercel.app"],
+        origin:[""],
         methods:["POST","GET"],
         credentials: true
     }
