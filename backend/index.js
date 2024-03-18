11effaf72eef59f2091419b0cc9e43e061e31755
Leo Jomar Reyes <105467483+leojomar01@users.noreply.github.com>
2024-03-18 21:35:44 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require ('cors');
-const todoRoutes = require('./routes/todoRoutes');
+const ToDoList = require('./models/ToDoList');
 
 
 
 const app = express();
-app.use(cors(
-    {
-        origin:["http://localhost:3000"],
-        methods:["POST","GET"],
-        credentials: true
-    }
-))
+app.use(cors())
 app.use( express.json())
 
-
-
-app.use("/",todoRoutes)
-
-
 mongoose.connect('mongodb+srv://leojomar01:442RXm249VzWh2Jb@cluster0.kbueheb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-//mongoose.connect('mongodb://localhost:27017/JAA')
 .then(()=>{
-    console.log("DB connected")
+    console.log("database connected")
+})
+
+app.post('/create',(req,res)=>{
+    console.log(req.body.values)
+    ToDoList.create(req.body.values)
+    .then (todolist => res.json(todolist))
+    .catch (err => res.json(err))
+})
+app.get('/getTodos',(req,res)=>{
+    ToDoList.find({}).sort({status:1,deadline:1})
+    .then (todos => {res.json(todos)})
+    .catch (err => res.json(err))
+})
+
+app.put('/updateRecord/:id',(req,res)=>{
+    const id = req.params.id
+    ToDoList.findByIdAndUpdate({_id:id},{status:req.body.status})
+    .then (todos => {res.json(todos)})
+    .catch (err => res.json(err))
+})
+
+app.put('/deleteRecord/:id',(req,res)=>{
+    const id = req.params.id
+    ToDoList.findByIdAndDelete({_id:id})
+    .then (todos => {res.json(todos)})
+    .catch (err => res.json(err))
 })
 
 
