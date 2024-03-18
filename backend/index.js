226a140dcf72dc01e46dd6aaebe091e40bd79a04
Leo Jomar Reyes <105467483+leojomar01@users.noreply.github.com>
2024-03-18 21:43:28 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,14 @@ const ToDoList = require('./models/ToDoList');
 const app = express();
 app.use(cors(
     {
-        origin:("https://jaa-e5cw.vercel.app"),
-        methods:("POST","GET"),
+        origin:["http://localhost:3000"],
+        methods:["POST","GET"],
         credentials: true
     }
 ))
 app.use( express.json())
 
 mongoose.connect('mongodb+srv://leojomar01:442RXm249VzWh2Jb@cluster0.kbueheb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=>{
-    console.log("database connected")
-})
 
 app.post('/create',(req,res)=>{
     console.log(req.body.values)
@@ -31,6 +28,9 @@ app.get('/getTodos',(req,res)=>{
     .then (todos => {res.json(todos)})
     .catch (err => res.json(err))
 })
+
+
+
 
 app.put('/updateRecord/:id',(req,res)=>{
     const id = req.params.id
