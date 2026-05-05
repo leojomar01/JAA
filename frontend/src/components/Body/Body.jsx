5fb4f2afd1dd4e57e30a059b522558dd12fd47a0
unknown <leojomar01@gmail.com>
2026-05-05 13:47:18 +0800

    fix link button

--- frontend/src/components/Body/Body.jsx
+++ frontend/src/components/Body/Body.jsx
@@ -81,7 +81,7 @@ function Body() {
     
                 <div>
                     <label htmlFor="desc">Messenger Link: </label>
-                    <a href={todo.desc}>Messagenger</a>
+                    <a href={todo.desc}>GC</a>
                     {/*<textarea name="desc" id="" cols="30" rows="5" defaultValue={todo.desc} readOnly></textarea>   */}
                 </div>
     
